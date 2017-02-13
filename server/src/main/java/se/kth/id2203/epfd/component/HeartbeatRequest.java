5df2f1a9cd8341468da4cc7f4953defabab0e889
MioRtia <miora.ralambotiana@gmail.com>
2017-02-13 18:38:52 +0100

    [EPFD] Redone without using Pp2p (with TCP)

--- server/src/main/java/se/kth/id2203/epfd/component/HeartbeatRequest.java
+++ server/src/main/java/se/kth/id2203/epfd/component/HeartbeatRequest.java
@@ -2,20 +2,21 @@ package se.kth.id2203.epfd.component;
 
 
 import se.kth.id2203.networking.NetAddress;
-import se.kth.id2203.pp2p.event.Pp2pDeliver;
+import se.kth.id2203.networking.NetMessage;
+import se.sics.kompics.network.Transport;
 
 import java.io.Serializable;
 
 /**
  * Created by ralambom on 11/02/17.
  */
-public class HeartbeatRequest extends Pp2pDeliver implements Serializable{
+public class HeartbeatRequest extends NetMessage implements Serializable{
 
     private static final long serialVersionUID = 2157271902369443307L;
     private int seqnum;
 
-    public HeartbeatRequest(NetAddress source, int seqnum) {
-        super(source);
+    public HeartbeatRequest(NetAddress source, NetAddress dest, int seqnum) {
+        super(source, dest, Transport.TCP);
         this.seqnum = seqnum;
     }
 
