a47890fb619f2df06d97b8bb444042a48e980a44
62maxime <maxime.dufour62@gmail.com>
2017-02-23 15:01:23 +0100

    [KVStore] Tests
    - test multiple read
    - two clients where one does 2 writes operation and the other read
    after each write
    - two clients that does write operation

---
+++ server/src/test/java/se/kth/id2203/simulation/kvstore/ScenarioClient.java
@@ -0,0 +1,192 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Lars Kroll <lkroll@kth.se>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package se.kth.id2203.simulation.kvstore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.kth.id2203.kvstore.*;
+import se.kth.id2203.networking.Message;
+import se.kth.id2203.networking.NetAddress;
+import se.kth.id2203.overlay.RouteMsg;
+import se.sics.kompics.*;
+import se.sics.kompics.network.Network;
+import se.sics.kompics.timer.Timer;
+
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.UUID;
+
+/**
+ * @author Lars Kroll <lkroll@kth.se>
+ */
+public class ScenarioClient extends ComponentDefinition {
+
+    final static Logger LOG = LoggerFactory.getLogger(ScenarioClient.class);
+    //******* Ports ******
+    protected final Positive<Network> net = requires(Network.class);
+    protected final Positive<Timer> timer = requires(Timer.class);
+    //******* Fields ******
+    private final NetAddress self = config().getValue("id2203.project.address", NetAddress.class);
+    private final NetAddress server = config().getValue("id2203.project.bootstrap-address", NetAddress.class);
+    private final SimulationResultMap res = SimulationResultSingleton.getInstance();
+    private final Map<UUID, String> pending = new TreeMap<>();
+    //******* Handlers ******
+    protected final Handler<Start> startHandler = new Handler<Start>() {
+
+        @Override
+        public void handle(Start event) {
+            int testNum = res.get("testNum", Integer.class);
+            if (testNum == 1) {
+                // only read operations
+                test1();
+            } else if (testNum == 2) {
+                // Write -> Read -> Write -> Read
+                test2();
+            } else if (testNum == 3) {
+                test3();
+            }
+
+        }
+    };
+    protected final ClassMatchedHandler<OpResponse, Message> responseHandler = new ClassMatchedHandler<OpResponse, Message>() {
+
+        @Override
+        public void handle(OpResponse content, Message context) {
+            LOG.debug("Got OpResponse: {}", content);
+            String key = pending.remove(content.id);
+            if (key != null) {
+                res.put(key, content.status.toString());
+            } else {
+                LOG.warn("ID {} was not pending! Ignoring response.", content.id);
+            }
+        }
+    };
+
+    protected final ClassMatchedHandler<GetResponse, Message> getResponseHandler = new ClassMatchedHandler<GetResponse, Message>() {
+
+        @Override
+        public void handle(GetResponse content, Message context) {
+            LOG.debug("Got GetResponse: {}", content);
+            String key = pending.remove(content.id);
+            if (key != null) {
+                if (content.getValue() == null) {
+                    res.put(key, "NOT_FOUND");
+                } else {
+                    int testNum = res.get("testNum", Integer.class);
+                    if (testNum == 2) {
+                        int client = config().getValue("id2203.project.client", Integer.class);
+                        res.put("client" + client, content.getValue().getValue());
+                    } else if (testNum == 3) {
+                        int client = config().getValue("id2203.project.client", Integer.class);
+                        res.put("client" + client, content.getValue().getValue());
+                        if (client == 2) {
+                            sendPut("write", client);
+                            res.put("client" + 1, "PUT");
+                        }
+                    } else {
+                        res.put(key, content.getValue().getValue());
+                    }
+
+                }
+
+            } else {
+                LOG.warn("ID {} was not pending! Ignoring response.", content.id);
+            }
+        }
+    };
+    protected final ClassMatchedHandler<PutResponse, Message> putResponseHandler = new ClassMatchedHandler<PutResponse, Message>() {
+
+        @Override
+        public void handle(PutResponse content, Message context) {
+            LOG.debug("Got PutResponse: {}", content);
+            String key = pending.remove(content.id);
+            if (key != null) {
+                res.put(key, content.status.toString());
+            } else {
+                LOG.warn("ID {} was not pending! Ignoring response.", content.id);
+            }
+        }
+    };
+
+    private void sendGet(String key) {
+        GetRequest op = new GetRequest(key);
+        RouteMsg rm = new RouteMsg(op.key, op); // don't know which partition is responsible, so ask the bootstrap server to forward it
+        trigger(new Message(self, server, rm), net);
+        pending.put(op.id, op.key);
+        LOG.info("Sending {}", op);
+        res.put(op.key, "SENT");
+    }
+
+    private void sendPut(String key, Integer value) {
+        PutRequest put = new PutRequest(key, new KVEntry(key.hashCode(), value));
+        RouteMsg rm = new RouteMsg(put.key, put); // don't know which partition is responsible, so ask the bootstrap server to forward it
+        trigger(new Message(self, server, rm), net);
+        pending.put(put.id, put.key);
+        res.put(put.key, "PUT");
+    }
+
+    private void test1() {
+        int messages = res.get("messages", Integer.class);
+        for (int i = 0; i < messages; i++) {
+            sendGet("test" + i);
+        }
+    }
+
+    private void test2() {
+        int type = config().getValue("id2203.project.type", Integer.class);
+        int client = config().getValue("id2203.project.client", Integer.class);
+        if (type == 0) {
+            sendGet("write");
+            res.put("client" + client, "GET");
+        } else {
+            sendPut("write", client);
+            res.put("client" + 2, "PUT");
+        }
+    }
+
+    private void test3() {
+        int client = config().getValue("id2203.project.client", Integer.class);
+        if (client == 2) {
+            sendGet("write");
+            res.put("client" + 2, "GET");
+        } else if (client == 1) {
+            int type = config().getValue("id2203.project.type", Integer.class);
+            if (type == 1) {
+                sendPut("write", client);
+                res.put("client" + 2, "PUT");
+            } else {
+                sendGet("write");
+                res.put("client" + 1, "GET");
+            }
+
+        }
+    }
+
+    {
+        subscribe(startHandler, control);
+        subscribe(responseHandler, net);
+        subscribe(getResponseHandler, net);
+        subscribe(putResponseHandler, net);
+    }
+}
---
+++ server/src/test/java/se/kth/id2203/simulation/kvstore/ScenarioGen.java
@@ -0,0 +1,305 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Lars Kroll <lkroll@kth.se>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package se.kth.id2203.simulation.kvstore;
+
+import se.kth.id2203.ParentComponent;
+import se.kth.id2203.networking.NetAddress;
+import se.sics.kompics.Init;
+import se.sics.kompics.network.Address;
+import se.sics.kompics.simulator.SimulationScenario;
+import se.sics.kompics.simulator.adaptor.Operation1;
+import se.sics.kompics.simulator.adaptor.Operation3;
+import se.sics.kompics.simulator.adaptor.distributions.extra.BasicIntSequentialDistribution;
+import se.sics.kompics.simulator.events.system.StartNodeEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Lars Kroll <lkroll@kth.se>
+ */
+public abstract class ScenarioGen {
+
+    private static final Operation1 startServerOp = new Operation1<StartNodeEvent, Integer>() {
+
+        @Override
+        public StartNodeEvent generate(final Integer self) {
+            return new StartNodeEvent() {
+                final NetAddress selfAdr;
+                final NetAddress bsAdr;
+
+                {
+                    try {
+                        selfAdr = new NetAddress(InetAddress.getByName("192.168.0." + self), 45678);
+                        bsAdr = new NetAddress(InetAddress.getByName("192.168.0.1"), 45678);
+                    } catch (UnknownHostException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+                @Override
+                public Address getNodeAddress() {
+                    return selfAdr;
+                }
+
+                @Override
+                public Class getComponentDefinition() {
+                    return ParentComponent.class;
+                }
+
+                @Override
+                public String toString() {
+                    return "StartNode<" + selfAdr.toString() + ">";
+                }
+
+                @Override
+                public Init getComponentInit() {
+                    return Init.NONE;
+                }
+
+                @Override
+                public Map<String, Object> initConfigUpdate() {
+                    HashMap<String, Object> config = new HashMap<>();
+                    config.put("id2203.project.address", selfAdr);
+                    if (self != 1) { // don't put this at the bootstrap server, or it will act as a bootstrap client
+                        config.put("id2203.project.bootstrap-address", bsAdr);
+                    }
+                    return config;
+                }
+            };
+        }
+    };
+
+    private static final Operation1 startClientOp = new Operation1<StartNodeEvent, Integer>() {
+
+        @Override
+        public StartNodeEvent generate(final Integer self) {
+            return new StartNodeEvent() {
+                final NetAddress selfAdr;
+                final NetAddress bsAdr;
+
+                {
+                    try {
+                        selfAdr = new NetAddress(InetAddress.getByName("192.168.1." + self), 45678);
+                        bsAdr = new NetAddress(InetAddress.getByName("192.168.0.1"), 45678);
+                    } catch (UnknownHostException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+                @Override
+                public Address getNodeAddress() {
+                    return selfAdr;
+                }
+
+                @Override
+                public Class getComponentDefinition() {
+                    return ScenarioClient.class;
+                }
+
+                @Override
+                public String toString() {
+                    return "StartClient<" + selfAdr.toString() + ">";
+                }
+
+                @Override
+                public Init getComponentInit() {
+                    return Init.NONE;
+                }
+
+                @Override
+                public Map<String, Object> initConfigUpdate() {
+                    HashMap<String, Object> config = new HashMap<>();
+                    config.put("id2203.project.address", selfAdr);
+                    config.put("id2203.project.bootstrap-address", bsAdr);
+                    config.put("id2203.project.client", self);
+                    config.put("id2203.project.type", 1);
+                    return config;
+                }
+            };
+        }
+    };
+
+    private static final Operation3 startReadClientOp = new Operation3<StartNodeEvent, Integer, Integer, Integer>() {
+
+        @Override
+        public StartNodeEvent generate(final Integer clientNumber, final Integer self, final Integer server) {
+            return new StartNodeEvent() {
+                final NetAddress selfAdr;
+                final NetAddress bsAdr;
+
+                {
+                    try {
+                        selfAdr = new NetAddress(InetAddress.getByName("192.168.2." + self), 45678);
+                        bsAdr = new NetAddress(InetAddress.getByName("192.168.0." + server), 45678);
+                    } catch (UnknownHostException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                }
+
+                @Override
+                public Address getNodeAddress() {
+                    return selfAdr;
+                }
+
+                @Override
+                public Class getComponentDefinition() {
+                    return ScenarioClient.class;
+                }
+
+                @Override
+                public String toString() {
+                    return "StartClient<" + selfAdr.toString() + ">";
+                }
+
+                @Override
+                public Init getComponentInit() {
+                    return Init.NONE;
+                }
+
+                @Override
+                public Map<String, Object> initConfigUpdate() {
+                    HashMap<String, Object> config = new HashMap<>();
+                    config.put("id2203.project.address", selfAdr);
+                    config.put("id2203.project.bootstrap-address", bsAdr);
+                    config.put("id2203.project.client", clientNumber);
+                    config.put("id2203.project.type", 0);
+                    return config;
+                }
+            };
+        }
+
+    };
+
+    public static SimulationScenario oneClient(final int servers) {
+        return new SimulationScenario() {
+            {
+                StochasticProcess startCluster = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(servers, startServerOp, new BasicIntSequentialDistribution(1));
+                    }
+                };
+
+                StochasticProcess startClients = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startClientOp, new BasicIntSequentialDistribution(1));
+                    }
+                };
+                startCluster.start();
+                startClients.startAfterTerminationOf(20000, startCluster);
+                terminateAfterTerminationOf(20000, startClients);
+            }
+        };
+    }
+
+    public static SimulationScenario twoClientsSameOperation(final int servers) {
+        return new SimulationScenario() {
+            {
+                StochasticProcess startCluster = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(servers, startServerOp, new BasicIntSequentialDistribution(1));
+                    }
+                };
+
+                StochasticProcess startWrite1 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startClientOp, new BasicIntSequentialDistribution(1));
+                    }
+                };
+                StochasticProcess startRead1 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startReadClientOp, new BasicIntSequentialDistribution(1),
+                                new BasicIntSequentialDistribution(2), new BasicIntSequentialDistribution(2));
+                    }
+                };
+                StochasticProcess startWrite2 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startClientOp, new BasicIntSequentialDistribution(2));
+                    }
+                };
+                StochasticProcess startRead2 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startReadClientOp, new BasicIntSequentialDistribution(2),
+                                new BasicIntSequentialDistribution(2), new BasicIntSequentialDistribution(2));
+                    }
+                };
+                startCluster.start();
+                startWrite1.startAfterTerminationOf(20000, startCluster);
+                startRead1.startAfterTerminationOf(1000, startWrite1);
+                startWrite2.startAfterTerminationOf(1000, startRead1);
+                startRead2.startAfterTerminationOf(1000, startWrite2);
+                terminateAfterTerminationOf(1000, startRead2);
+            }
+        };
+    }
+
+    public static SimulationScenario twoClientsAlternatif(final int servers) {
+        return new SimulationScenario() {
+            {
+                StochasticProcess startCluster = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(servers, startServerOp, new BasicIntSequentialDistribution(1));
+                    }
+                };
+
+                StochasticProcess startWrite1 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startClientOp, new BasicIntSequentialDistribution(1));
+                    }
+                };
+                StochasticProcess startRead1 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startReadClientOp, new BasicIntSequentialDistribution(2),
+                                new BasicIntSequentialDistribution(1), new BasicIntSequentialDistribution(2));
+                    }
+                };
+                StochasticProcess startRead2 = new StochasticProcess() {
+                    {
+                        eventInterArrivalTime(constant(1000));
+                        raise(1, startReadClientOp, new BasicIntSequentialDistribution(1),
+                                new BasicIntSequentialDistribution(2), new BasicIntSequentialDistribution(1));
+                    }
+                };
+                startCluster.start();
+                startWrite1.startAfterTerminationOf(20000, startCluster);
+                startRead1.startAfterTerminationOf(1000, startWrite1);
+                startRead2.startAfterTerminationOf(2000, startRead1);
+                terminateAfterTerminationOf(1000, startRead2);
+            }
+        };
+    }
+}
